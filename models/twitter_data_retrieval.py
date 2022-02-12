5ac1ba4e1dba04ee9f83b52ec1d482e7a8fc4aa5
Jonas Heim <jonasheim@linux4.cs.uchicago.edu>
2022-02-12 13:03:36 -0600

    Twitter Proof of Concept

--- models/twitter_data_retrieval.py
+++ models/twitter_data_retrieval.py
@@ -6,7 +6,28 @@ Script to get Twitter Data
 import twint
 # Configure
 c = twint.Config()
-c.Username = "MichelleObama"
+c.Store_json = True
+c.Output = "test.json"
+c.Search = "wildfires AND California"
+c.Since = "2010-01-01"
+c.Until = "2020-12-31"
+#c.Location = True
+#c.Geo = (34.052235, -118.243683, 100)
+c.Limit = 10
+#c.Format = "tweet: {tweet} | date: {date}"
+"""
+c.Format = '''date: {date} |
+    time: {time} |
+    place: {place} |
+    tweet: {tweet} |
+    language: {language} |
+    retweet: {retweet} |
+    near: {near} |
+    geo: {geo} |
+    source: {source} |
+    retweet_date: {retweet_date}
+    '''
+"""
 
 # Run
 twint.run.Search(c)
