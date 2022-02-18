b59ace3b9337a012e41b67dabd1eb12938435e4e
Jonas Heim <jonasheim@linux4.cs.uchicago.edu>
2022-02-18 12:17:50 -0600

    Advances in API

--- models/twitter_api.py
+++ models/twitter_api.py
@@ -5,6 +5,7 @@ Script to get Twitter data using tweepy
 '''
 import tweepy
 import api_access
+import datetime
 
 auth = tweepy.OAuthHandler(api_access.consumer_key, api_access.consumer_secret)
 auth.set_access_token(api_access.access_token, api_access.access_token_secret)
@@ -13,22 +14,15 @@ api = tweepy.API(auth)
 
 client = tweepy.Client(bearer_token = api_access.bearer_token)
 
-# tweets = client.search_all_tweets(
-#     query = 'california wildfires',
-#     max_results = 10)
-# for tweet in tweets.data:
-#     print(tweet)
-#     print('\n')
-
-# date, coordinates, text
-
-#Get the tweets
 tweets = tweepy.Cursor(             #geocode
     api.search_tweets,
-    q = 'california wildfires',
-    lang = 'en').items(100000)
+    q = f'california wildfires',
+    lang = 'en').items(10000)
 
 #Test to see if some tweets have coordinates
 for tweet in tweets:
-    if tweet.coordinates != None:
-        print(tweet.coordinates)
+    if tweet.place is not None:
+        print(tweet.created_at)
+        print(tweet.text)
+        print(tweet.place)
+        print('\n')
