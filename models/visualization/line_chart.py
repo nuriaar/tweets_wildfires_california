02e7fbbdaee9ee8e3f7744028a6f67586529898d
Jonas Heim <jonasheim@linux4.cs.uchicago.edu>
2022-02-27 12:24:23 -0600

    JH: viz improved

--- models/visualization/line_chart.py
+++ models/visualization/line_chart.py
@@ -1,30 +1,8 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import glob
 
-wildfire_data = pd.read_csv('../../data/Cal_Fires.csv')
-wildfire_data.rename(columns={
-    'OBJECTID': 'object_id',
-    'YEAR_': 'year',
-    'STATE': 'state',
-    'AGENCY': 'agency',
-    'UNIT_ID': 'unit_id',
-    'FIRE_NAME': 'fire_name',
-    'INC_NUM': 'inc_num',
-    'ALARM_DATE': 'alarm_date',
-    'CONT_DATE': 'containment_date',
-    'CAUSE': 'cause',
-    'COMMENTS': 'comments',
-    'REPORT_AC': 'report_ac',
-    'GIS_ACRES': 'gis_acres',
-    'C_METHOD': 'c_method',
-    'OBJECTIVE': 'objective',
-    'FIRE_NUM': 'fire_num',
-    'Shape__Area': 'shape_area',
-    'Shape__Length': 'shape_length'},inplace = True)
-wildfire_data.dropna(axis = 0, how = 'any', subset = ['year', 'state', 'fire_name', 'alarm_date', 'containment_date', 'gis_acres', 'shape_area', 'shape_length'], inplace = True)
-wildfire_data['alarm_date'] = pd.to_datetime(wildfire_data['alarm_date'], yearfirst = True, format = '%w', infer_datetime_format=True)
-wildfire_data['containment_date'] = pd.to_datetime(wildfire_data['containment_date'], yearfirst = True, format = '%w', infer_datetime_format=True)
 
 while True:
     year = input('Enter a year (2010-2020): ')
@@ -38,18 +16,61 @@ while True:
     else:
         print('please enter a year within the specified range')
 
-relevant_data = wildfire_data[wildfire_data['year'] == year].reset_index(drop = True)
-relevant_data = relevant_data.loc[:, ['alarm_date', 'containment_date', 'gis_acres']]
-week_dict = {'week': [week for week in range(1, 52 + 1)], 'acres': [0]*52}
+while True:
+    season = input('Season (fire, off, both): ')
+    if season in ['fire', 'off', 'both']:
+        break
+    else:
+        print('please enter eith "fire", "off", or "both"')
+
+wildfire_data = pd.read_csv('../../data/Cal_Fires.csv', usecols = ['YEAR_', 'STATE', 'ALARM_DATE', 'CONT_DATE', 'GIS_ACRES'])
+wildfire_data.rename(columns = {
+    'YEAR_': 'year',
+    'STATE': 'state',
+    'ALARM_DATE': 'alarm_date',
+    'CONT_DATE': 'containment_date',
+    'GIS_ACRES': 'gis_acres',
+    },inplace = True)
+wildfire_data.dropna(axis = 0, how = 'any', subset = ['year', 'state', 'alarm_date', 'containment_date', 'gis_acres'], inplace = True)
+wildfire_data['alarm_date'] = pd.to_datetime(wildfire_data['alarm_date'], yearfirst = True, format = '%w', infer_datetime_format=True)
+wildfire_data['containment_date'] = pd.to_datetime(wildfire_data['containment_date'], yearfirst = True, format = '%w', infer_datetime_format=True)
+
+twitter_filenames = glob.glob(f'../../data/{year}*'.format('csv'))
+twitter_data = pd.concat([pd.read_csv(file, usecols = ['Date']) for file in twitter_filenames])
+twitter_data.rename(columns = {'Date': 'date'}, inplace = True)
+twitter_data['date'] = pd.to_datetime(twitter_data['date'], yearfirst = True, format = '%w', infer_datetime_format=True)
+
+relevant_wildfire_data = wildfire_data[wildfire_data['year'] == year].reset_index(drop = True)                 ### filter on state as well?
+relevant_wildfire_data = relevant_wildfire_data.loc[:, ['alarm_date', 'containment_date', 'gis_acres']]
+week_dict = {'week': [week for week in range(1, 52 + 1)], 'acres': [0]*52, 'nb_tweets': [0]*52}
 data = pd.DataFrame(week_dict)
-for idx, row in enumerate(relevant_data.itertuples()):
+for idx, row in enumerate(relevant_wildfire_data.itertuples()):
     start_week, end_week, acres = row[1].week, row[2].week, row[3]
     for week in range(start_week, end_week):
         data.loc[week - 1, 'acres'] += acres         ### gis_acres, add acres to every week burned
+for row in twitter_data.itertuples():
+    week = row[1].week
+    if 1 <= week <= 52:
+        data.loc[week - 1, 'nb_tweets'] += 1
+
+if season == 'fire':
+    relevant_weeks = [w for w in range(18, 45)]
+elif season == 'off':                                ### Probably take out the offseason
+    relevant_weeks = [w for w in range(1, 53) if w not in range(18, 45)]
+else:
+    relevant_weeks = [w for w in range(1, 53)]
+
+data = data[data['week'].isin(relevant_weeks)]
 print(data)                 ### delete
-sns.set()
-x = data['week']
-y = data['acres']
-sns.lineplot(x = x, y = y)
+
+fig, ax1 = plt.subplots()
+ax1.plot('week', 'acres', data = data, color = 'red')
+ax2 = ax1.twinx()
+ax2.plot('week', 'nb_tweets', data = data, color = 'blue')
+plt.title(f'Acres Burned and Tweet Intensity in {year}')
+ax1.set_xlabel('Week')
+ax1.set_ylabel('Burned Acres')
+ax2.set_ylabel('Number of Tweets')
+ax1.yaxis.label.set_color('red')
+ax2.yaxis.label.set_color('blue')
 plt.savefig('test.png')
-plt.show()
