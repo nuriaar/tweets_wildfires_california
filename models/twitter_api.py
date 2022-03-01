f35c7b5c16563785f3967eca2ea89ed7d455ac1c
Sergio Olalla Ubierna <sergiou@linux7.cs.uchicago.edu>
2022-03-01 12:33:11 -0600

    api

--- models/twitter_api.py
+++ models/twitter_api.py
@@ -1,36 +1,79 @@
-'''
-Pavan and Jonas
-
-Script to get Twitter data using tweepy
-'''
 import tweepy
-from tweepy import Client
 import api_access
-import datetime
+import time
+import geopy
+from geopy.geocoders import Nominatim
+import csv
 
-# auth = tweepy.OAuthHandler(api_access.consumer_key, api_access.consumer_secret)
-# auth.set_access_token(api_access.access_token, api_access.access_token_secret)
+def extract_calfire_tweets(start_date, end_date):
+    '''
+    Format: 2019-01-01T00:00:00Z
+    '''
+    client = tweepy.Client(bearer_token = api_access.bearer_token)
 
-# api = tweepy.API(auth)
+    tweets = []
 
-# client = tweepy.Client(bearer_token = api_access.bearer_token)
+    for tweet in tweepy.Paginator(client.search_all_tweets,
+        query = 'wildfires california', 
+        start_time = start_date, 
+        end_time = end_date, 
+        tweet_fields = ['text', 'created_at', 'geo'], 
+        place_fields = ['place_type', 'geo'],
+        expansions = 'geo.place_id', 
+        max_results=500):
 
-client = tweepy.Client(
-    consumer_key = api_access.consumer_key,
-    consumer_secret = api_access.consumer_secret,
-    access_token = api_access.access_token,
-    access_token_secret = api_access.access_token_secret
-)
+        time.sleep(10)
+        tweets.append(tweet)
+    
+    return tweets
 
-tweets = tweepy.Cursor(             #geocode
-    client.search_tweets,
-    q = f'california wildfires',
-    lang = 'en').items(10000)
+def extract_tweets_info(list_of_tweet_responses, output_name):
+    '''
+    Extract and update to sql database
+    '''
+    location_coord = {}
+    coord_state = {} #need to do this so open maps doesn't return error
+    location_state = {}
 
-#Test to see if some tweets have coordinates
-for tweet in tweets:
-    if tweet.place is not None:
-        print(tweet.created_at)
-        print(tweet.text)
-        print(tweet.place)
-        print('\n')
+    results = []
+    count = 0
+
+    path = "/home/sergiou/capp30122/proj-larry_on_fire/proj-larry_on_fire/data/" + output_name
+
+    for response in list_of_tweet_responses:
+        if 'places' in response.includes.keys():
+            for place in response.includes['places']:
+                coordinates = place.geo['bbox']
+                lat = str((coordinates[1] + coordinates[3])/ 2)
+                lon = str((coordinates[0] + coordinates[2])/ 2)
+                location_coord[place.id] = lat+","+lon
+
+    geolocator = Nominatim(user_agent="geoapiExercises")
+
+    for key, location_str in location_coord.items():
+
+        if not location_str in coord_state.keys():
+            location = geolocator.reverse(location_str)
+
+            if location is not None and 'state' in location.raw['address'].keys():
+                state = location.raw['address']['state']
+                coord_state[location_str] = state
+        else:
+            location_state[key] = coord_state[location_str]
+
+    with open(path, "w") as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=",")
+        spamwriter.writerow(['Id', 'Date', 'Text', 'State'])
+                    
+        for response in list_of_tweet_responses:       
+            for tweet in response.data:
+                place_id = ''
+                if tweet['geo'] is not None:
+                    place_id = tweet['geo']['place_id']
+
+                tweet_id = tweet.id
+                date = tweet.created_at.strftime("%Y-%m-%d")
+                text = tweet.text
+                state = location_state.get(place_id, None)
+                spamwriter.writerow([tweet_id, date, text, state])
+    
---
+++ models/visualization/line_chart.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import glob
+
+
+while True:
+    year = input('Enter a year (2010-2020): ')
+    try:
+        year = int(year)
+    except:
+        print('please input a valid year')
+        continue
+    if 2010 <= year <= 2020:
+        break
+    else:
+        print('please enter a year within the specified range')
+
+while True:
+    season = input('Season (fire, off, both): ')
+    if season in ['fire', 'off', 'both']:
+        break
+    else:
+        print('please enter eith "fire", "off", or "both"')
+
+wildfire_data = pd.read_csv('../../data/Cal_Fires.csv', usecols = ['YEAR_', 'STATE', 'ALARM_DATE', 'CONT_DATE', 'GIS_ACRES'])
+wildfire_data.rename(columns = {
+    'YEAR_': 'year',
+    'STATE': 'state',
+    'ALARM_DATE': 'alarm_date',
+    'CONT_DATE': 'containment_date',
+    'GIS_ACRES': 'gis_acres',
+    },inplace = True)
+wildfire_data.dropna(axis = 0, how = 'any', subset = ['year', 'state', 'alarm_date', 'containment_date', 'gis_acres'], inplace = True)
+wildfire_data['alarm_date'] = pd.to_datetime(wildfire_data['alarm_date'], yearfirst = True, format = '%w', infer_datetime_format=True)
+wildfire_data['containment_date'] = pd.to_datetime(wildfire_data['containment_date'], yearfirst = True, format = '%w', infer_datetime_format=True)
+
+twitter_filenames = glob.glob(f'../../data/{year}*'.format('csv'))
+twitter_data = pd.concat([pd.read_csv(file, usecols = ['Date']) for file in twitter_filenames])
+twitter_data.rename(columns = {'Date': 'date'}, inplace = True)
+twitter_data['date'] = pd.to_datetime(twitter_data['date'], yearfirst = True, format = '%w', infer_datetime_format=True)
+
+relevant_wildfire_data = wildfire_data[wildfire_data['year'] == year].reset_index(drop = True)                 ### filter on state as well?
+relevant_wildfire_data = relevant_wildfire_data.loc[:, ['alarm_date', 'containment_date', 'gis_acres']]
+week_dict = {'week': [week for week in range(1, 52 + 1)], 'acres': [0]*52, 'nb_tweets': [0]*52}
+data = pd.DataFrame(week_dict)
+for idx, row in enumerate(relevant_wildfire_data.itertuples()):
+    start_week, end_week, acres = row[1].week, row[2].week, row[3]
+    for week in range(start_week, end_week):
+        data.loc[week - 1, 'acres'] += acres         ### gis_acres, add acres to every week burned
+for row in twitter_data.itertuples():
+    week = row[1].week
+    if 1 <= week <= 52:
+        data.loc[week - 1, 'nb_tweets'] += 1
+
+if season == 'fire':
+    relevant_weeks = [w for w in range(18, 45)]
+elif season == 'off':                                ### Probably take out the offseason
+    relevant_weeks = [w for w in range(1, 53) if w not in range(18, 45)]
+else:
+    relevant_weeks = [w for w in range(1, 53)]
+
+data = data[data['week'].isin(relevant_weeks)]
+print(data)                 ### delete
+
+fig, ax1 = plt.subplots()
+ax1.plot('week', 'acres', data = data, color = 'red')
+ax2 = ax1.twinx()
+ax2.plot('week', 'nb_tweets', data = data, color = 'blue')
+plt.title(f'Acres Burned and Tweet Intensity in {year}')
+ax1.set_xlabel('Week')
+ax1.set_ylabel('Burned Acres')
+ax2.set_ylabel('Number of Tweets')
+ax1.yaxis.label.set_color('red')
+ax2.yaxis.label.set_color('blue')
+plt.savefig('test.png')
