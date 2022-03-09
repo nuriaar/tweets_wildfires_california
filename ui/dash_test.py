c4f36cff91a4f701ea02fb54a2afdb55edc59bf2
Núria Adell Raventós <nuriaadellraventos@Nurias-MacBook-Air-2.local>
2022-03-09 16:28:24 -0600

    reorganized dash test

--- ui/dash_test.py
+++ ui/dash_test.py
@@ -10,24 +10,34 @@ from resources.utils import read_tweets_data, filter_coord_data, read_coord_data
 from ui.tweets_analysis_viz import create_wordcloud, create_lda_table
 from ui.map_viz import map_wildfires
 
+# LOAD DATA WHEN LOADING DASH - NO NEED TO REDO WITH CHANGING INPUTS
+
 tweets_data_filepath = "data/twitter_data/"
 tweets_state_filename = "tweets_state.csv"
 tweets_sample_filename = "sample_clean_data.csv"
 
 tweets_state_data = read_tweets_data(tweets_data_filepath + tweets_state_filename)
 tweets_sample_data = read_tweets_data(tweets_data_filepath + tweets_sample_filename)
+coord_data = read_coord_data()
 
+
+# FILTER DATA ACCORDING TO USER INPUTS 
 # !!! get inputs for year, fire_season, state_info
+
 if state_info:
     tweets = filter_coord_data(tweets_state_data, year, state_info, fire_season)
 else:
     tweets = filter_coord_data(tweets_sample_data, year, state_info, fire_season)
 
-# create wordcloud and save it as .png
-create_wordcloud(tweets)
-# create figure of lda table
-lda_table = create_lda_table(tweets)
+filtered_coord_data = filter_coord_data(coord_data, year, fire_season)
 
+# CREATE DATA VISUALIZATIONS
+create_wordcloud(tweets)
+lda_table = create_lda_table(tweets)
+map = map_wildfires(filtered_coord_data, year, fire_season)
+
+
+# DASH 
 
 # To show wordcloud –––––––––––––––––
 
@@ -49,13 +59,7 @@ if __name__ == '__main__':
 # To show table see end of https://plotly.com/python/table/ –––––––––––––––––
 
 
-
 # To show map –––––––––––––––––
-
-coord_data = read_coord_data()
-
-# get year and fire_season inputs
-map = map_wildfires(coord_data, year, fire_season)
 
 app = dash.Dash()
 app.layout = html.Div([
