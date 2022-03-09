82758b7e72f17d7538bc412a71812d8920cf12dd
Núria Adell Raventós <nuriaadellraventos@Nurias-MacBook-Air-2.local>
2022-03-08 21:01:34 -0600

    fire_season boolean

--- resources/clean_wildfire_coordinate_data.py
+++ resources/clean_wildfire_coordinate_data.py
@@ -20,7 +20,7 @@ wildfires_data = pd.DataFrame(columns = cols)
 
 years = ["2015", "2016", "2017", "2018", "2019", "2020", "2021"]
 
-for year1 in ["2015"]:
+for year1 in years:
 
     i = 0
     for fire in fires_gj["features"]:
@@ -38,53 +38,30 @@ for year1 in ["2015"]:
 
                 alarm_date = properties["ALARM_DATE"]
                 cont_date = properties["CONT_DATE"]
-                if alarm_date is not None:
-                    alarm_date = datetime.strptime(alarm_date, "%Y-%m-%dT%H:%M:%SZ")
-                if cont_date is not None:
-                    cont_date = datetime.strptime(cont_date, "%Y-%m-%dT%H:%M:%SZ")
-                
-                fire_season = False
-                if cont_date.dt.month >= 5 and alarm_date.dt.month < 11:
-                    fire_season = True
-                
-                alarm_date = alarm_date.date()
-                cont_date = cont_date.date()
 
-                for polygons in fire["geometry"]["coordinates"]:
-
-                    if isinstance(polygons[0][0], float):
-
-                        for i, (lon, lat) in enumerate(polygons):
-
-                            if i%5 == 0:
-
-                                # Append coordinates
-                                values = [fire_name, gis_acres, year, alarm_date, cont_date, fire_season, lat, lon]
-                                new_row = pd.DataFrame([values], columns = cols)
-                                wildfires_data = pd.concat([wildfires_data, new_row], ignore_index=True)
-
-                        # Link back to initial coordinate
-                        values = [fire_name, gis_acres, year, alarm_date, cont_date, fire_season,
-                                polygons[0][1], polygons[0][0]]
-                        new_row = pd.DataFrame([values], columns = cols)
-                        wildfires_data = pd.concat([wildfires_data, new_row], ignore_index=True)
-
-                        # Space between polygons
-                        values = [None] * len(cols)
-                        new_row = pd.DataFrame([values], columns = cols)
-                        wildfires_data = pd.concat([wildfires_data, new_row], ignore_index=True)
+                if alarm_date is not None and cont_date is not None:
+                    if alarm_date is not None:
+                        alarm_date = datetime.strptime(alarm_date, "%Y-%m-%dT%H:%M:%SZ").date()
+                    if cont_date is not None:
+                        cont_date = datetime.strptime(cont_date, "%Y-%m-%dT%H:%M:%SZ").date()
                     
-                    else:
+                    fire_season = False
+                    if cont_date.month >= 5 & alarm_date.month < 11:
+                        fire_season = True
 
-                        for polygon in polygons:
+                    for polygons in fire["geometry"]["coordinates"]:
 
-                            for lon, lat in polygon:
+                        if isinstance(polygons[0][0], float):
 
-                                # Append coordinates
-                                values = [fire_name, gis_acres, year, alarm_date, cont_date, fire_season, lat, lon]
-                                new_row = pd.DataFrame([values], columns = cols)
-                                wildfires_data = pd.concat([wildfires_data, new_row], ignore_index=True)
-                            
+                            for i, (lon, lat) in enumerate(polygons):
+
+                                if i%5 == 0:
+
+                                    # Append coordinates
+                                    values = [fire_name, gis_acres, year, alarm_date, cont_date, fire_season, lat, lon]
+                                    new_row = pd.DataFrame([values], columns = cols)
+                                    wildfires_data = pd.concat([wildfires_data, new_row], ignore_index=True)
+
                             # Link back to initial coordinate
                             values = [fire_name, gis_acres, year, alarm_date, cont_date, fire_season,
                                     polygons[0][1], polygons[0][0]]
@@ -95,6 +72,28 @@ for year1 in ["2015"]:
                             values = [None] * len(cols)
                             new_row = pd.DataFrame([values], columns = cols)
                             wildfires_data = pd.concat([wildfires_data, new_row], ignore_index=True)
+                        
+                        else:
+
+                            for polygon in polygons:
+
+                                for lon, lat in polygon:
+
+                                    # Append coordinates
+                                    values = [fire_name, gis_acres, year, alarm_date, cont_date, fire_season, lat, lon]
+                                    new_row = pd.DataFrame([values], columns = cols)
+                                    wildfires_data = pd.concat([wildfires_data, new_row], ignore_index=True)
+                                
+                                # Link back to initial coordinate
+                                values = [fire_name, gis_acres, year, alarm_date, cont_date, fire_season,
+                                        polygons[0][1], polygons[0][0]]
+                                new_row = pd.DataFrame([values], columns = cols)
+                                wildfires_data = pd.concat([wildfires_data, new_row], ignore_index=True)
+
+                                # Space between polygons
+                                values = [None] * len(cols)
+                                new_row = pd.DataFrame([values], columns = cols)
+                                wildfires_data = pd.concat([wildfires_data, new_row], ignore_index=True)
 
     preprocessed_data_filename = "../data/wildfire_coordinate_data/clean_wildfires_data_" + year1 + ".csv"
     wildfires_data.to_csv(preprocessed_data_filename, index=False)
