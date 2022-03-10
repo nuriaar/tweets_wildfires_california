b07bb661029f3ecd11ce3219a098d7459055c7a4
Jheim12 <jonjonas.heim@gmail.com>
2022-03-10 10:18:22 -0600

    minor bug fix

--- ui/line_chart.py
+++ ui/line_chart.py
@@ -26,7 +26,7 @@ import dash_html_components as html
 app = dash.Dash()
 app.layout = html.Div([
     dcc.Dropdown(id = 'year', options = [{'label': x, 'value': x} for x in [2015, 2016, 2017, 2018, 2019, 2020, 2021]], value = 2021),
-    dcc.RadioItems(id = 'fire_season', options = [{'label': 'Entire Year', 'value': False}, {'label': 'Fire Season Only', 'value': True}], value = 'Entire Year'),
+    dcc.RadioItems(id = 'fire_season', options = [{'label': 'Entire Year', 'value': False}, {'label': 'Fire Season Only', 'value': True}], value = False),
     dcc.Graph(id = 'line_chart')
 ])
 
