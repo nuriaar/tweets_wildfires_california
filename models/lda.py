3d1a150de26e060bc5171f47b83c7fbb42e5f8a0
Sergio Olalla Ubierna <sergiou@linux7.cs.uchicago.edu>
2022-03-11 11:28:33 -0600

    cleaning up

--- models/lda.py
+++ models/lda.py
@@ -4,7 +4,7 @@ import gensim
 
 def retrieve_topics(tweets):
     '''
-    Retrieve main 3 topics from tweets (7 words per topic) and visualize a table
+    Retrieve main 3 topics from tweets (7 words per topic)
 
     Inputs:
         tweets: list of tweets
