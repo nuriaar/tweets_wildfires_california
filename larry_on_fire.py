05c800a9c78179044b468daf299b824227d407fe
Pavan kumar Prathuru <pavanprathuru@linux6.cs.uchicago.edu>
2022-03-11 18:31:24 -0600

    latest functional version

--- larry_on_fire.py
+++ larry_on_fire.py
@@ -5,7 +5,8 @@ from dash import Input, Output
 import plotly.express as px
 import base64
 
-from resources.utils import read_tweets_data, filter_coord_data, read_coord_data, filter_tweets_data
+from resources.utils import read_tweets_data, filter_coord_data,\
+     read_coord_data, filter_tweets_data
 from ui.tweets_analysis_viz import create_wordcloud, create_lda_table
 from ui.map_viz import map_wildfires
 from ui.line_chart import line_chart
@@ -15,29 +16,35 @@ tweets_data_filepath = "data/twitter_data/"
 tweets_state_filename = "tweets_state.csv"
 tweets_sample_filename = "sample_clean_data.csv"
 
-tweets_state_data = read_tweets_data(tweets_data_filepath + tweets_state_filename)
-tweets_sample_data = read_tweets_data(tweets_data_filepath + tweets_sample_filename)
+tweets_state_data = read_tweets_data(tweets_data_filepath + \
+    tweets_state_filename)
+tweets_sample_data = read_tweets_data(tweets_data_filepath + \
+    tweets_sample_filename)
 coord_data = read_coord_data()
 
 
 ### CREATING INITIAL DATA VISUALIZATIONS
 
 ## Geo map
-filtered_coord_data = filter_coord_data(coord_data, year = 2020, fire_season = True)
-geo_map = map_wildfires(filtered_coord_data, year = 2020, fire_season = True)
+filtered_coord_data = filter_coord_data(coord_data, year = 2020, \
+    fire_season = True)
+geo_map = map_wildfires(filtered_coord_data, year = 2020, \
+    fire_season = True)
 
 ## Tweet line chart
 tweet_plot = line_chart(year=2020, fire_season = True)
 
 
 ## Word cloud and LDA
-filtered_tweets = filter_tweets_data(tweets_sample_data, state_info = False, year = 2020, fire_season = True)
+filtered_tweets = filter_tweets_data(tweets_sample_data, state_info = False, \
+    year = 2020, fire_season = True)
 
 # Word cloud
 create_wordcloud(filtered_tweets) # Word Cloud
 image_filepath = "ui/"
 image_filename = 'tweets_wordcloud.png'
-encoded_image = base64.b64encode(open(image_filepath + image_filename, 'rb').read())
+encoded_image = base64.b64encode(open(image_filepath + image_filename,\
+     'rb').read())
 
 # LDA
 lda_table = create_lda_table(filtered_tweets)
@@ -49,7 +56,8 @@ app = dash.Dash(__name__)
 # Defining the app layout
 app.layout = html.Div(
     children=[
-        html.H2('Project Larry on Fire - Analyzing social media data for wildfires in California, USA'),
+        html.H2('Project Larry on Fire - Analyzing social media data \
+            for wildfires in California, USA'),
         html.Div(className='Larry_Page',
             children=[
                 html.Table(
@@ -58,7 +66,8 @@ app.layout = html.Div(
                             html.P('''Select a year:''')
                         ),
                         html.Td(
-                            dcc.Dropdown(id = 'fire_year', options = [{'label': x, 'value': x} \
+                            dcc.Dropdown(id = 'fire_year', options = \
+                                [{'label': x, 'value': x} \
                                 for x in [2015, 2016, 2017, 2018, 2019, 2020]], \
                                 value = 2020)
                         )]
@@ -69,8 +78,10 @@ app.layout = html.Div(
                             ),
                             html.Td(
                                 dcc.RadioItems(id = 'fire_season', \
-                                    options = [{'label': 'Entire Year', 'value': False}, \
-                                    {'label': 'Fire Season Only', 'value': True}], value = True)
+                                    options = [{'label': 'Entire Year', \
+                                        'value': False}, {'label': 'Fire \
+                                            Season Only', 'value': True}],\
+                                                 value = True)
                             )]
                     ),
                     html.Tr(
@@ -79,9 +90,12 @@ app.layout = html.Div(
                             ),
                             html.Td(
                                 dcc.RadioItems(id = 'state_info', \
-                                    options = [{'label': 'Within state', 'value': "in"}, \
-                                    {'label': 'Outside state', 'value': "out"},\
-                                    {'label': 'All USA', 'value': False}], value = False, inline=True)
+                                    options = [{'label': 'Within state', \
+                                        'value': "in"}, \
+                                    {'label': 'Outside state', \
+                                        'value': "out"},\
+                                    {'label': 'All USA', 'value': False}], \
+                                        value = False, inline=True)
                             )]
                     )]
             ),
@@ -104,7 +118,9 @@ app.layout = html.Div(
             html.Table(
                     [html.Tr(
                             [html.Td(
-                                html.Div([html.Img(id="word_cloud", src='data:image/png;base64,{}'.format(encoded_image.decode()))
+                                html.Div([html.Img(id="word_cloud", \
+                                    src='data:image/png;base64,{}'.format(\
+                                        encoded_image.decode()))
                                 ])
                             )]
                     ),
@@ -146,11 +162,13 @@ def update_tweet_line_chart(year, fire_season):
     dash.dependencies.Input('fire_season', 'value'),
     dash.dependencies.Input('state_info', 'value')])
 def update_word_cloud(year, fire_season, state_info):
-    filtered_tweets = filter_tweets_data(tweets_sample_data, state_info = state_info, year = year, fire_season = fire_season)
-    create_wordcloud(filtered_tweets) # Word Cloud
+    filtered_tweets = filter_tweets_data(tweets_sample_data, \
+        state_info = state_info, year = year, fire_season = fire_season)
+    create_wordcloud(filtered_tweets)
     image_filepath = "ui/"
     image_filename = 'tweets_wordcloud.png'
-    encoded_image = base64.b64encode(open(image_filepath + image_filename, 'rb').read())
+    encoded_image = base64.b64encode(open(image_filepath + \
+        image_filename, 'rb').read())
     return 'data:image/png;base64,{}'.format(encoded_image.decode())
 
 
@@ -161,7 +179,8 @@ def update_word_cloud(year, fire_season, state_info):
     dash.dependencies.Input('fire_season', 'value'),
     dash.dependencies.Input('state_info', 'value')])
 def update_lda_chart(year, fire_season, state_info):
-    filtered_tweets = filter_tweets_data(tweets_sample_data, state_info = state_info, \
+    filtered_tweets = filter_tweets_data(tweets_sample_data, \
+        state_info = state_info, \
         year = year , fire_season = fire_season)
     return create_lda_table(filtered_tweets)
 
